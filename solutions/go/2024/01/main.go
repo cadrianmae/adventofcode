e1801a463a4e77dbc1b33c8db8286b6568aca4dc
C Adrian Mae <45900436+cadrianmae@users.noreply.github.com>
2024-12-06 03:43:34 +0000

    Never gonna say goodbye

---
+++ solutions/go/2024/01/main.go
@@ -0,0 +1,31 @@
+package main
+/*
+* ElfScript Brigade
+*
+* Advent Of Code 2024 Day 01
+* Rust Solution
+*
+* Day 1: Historian Hysteria
+*
+* https://adventofcode.com/2024/day/1
+*
+*/
+
+import (
+  "github.com/luxedo/esb_fireplace-go"
+)
+
+func solve_pt1(input_data string, args []string) (interface{}, error) {
+	return 25, nil
+}
+
+
+func solve_pt2(input_data string, args []string) (interface{}, error) {
+	return "December", nil
+}
+
+func main() {
+  // 🎅🎄❄️☃️🎁🦌
+  // Bright christmas lights HERE
+  esb_fireplace.V1Run(solve_pt1, solve_pt2)
+}
